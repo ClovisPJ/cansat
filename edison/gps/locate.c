cc3a751bd4dce42c8f39d65dc17574277dee4314
ClovisPJ <cpj234@gmail.com>
2016-01-27 12:57:11 +0000

    GPS small changes

--- edison/gps/locate.c
+++ edison/gps/locate.c
@@ -11,23 +11,23 @@ int main() {
 
   while (1) {
     if (gps_fix()) {
-      gps__get_nmea("$GPRMC");
-      gsl_vector *loc = gps_location;
-      gsl_vector_fprintf(stdout, loc, "%f");
+      gps_get_nmea("$GPRMC");
+      gps_get_nmea("$GPGGA");
 
+      gsl_vector *loc = gps_location;
       exttm = gps_time;
       time = (struct tm){exttm.tm_sec, exttm.tm_min, exttm.tm_hour, exttm.tm_mday, exttm.tm_mon, exttm.tm_year, exttm.tm_wday, exttm.tm_yday};
-      printf("The time is: %s\n",asctime(&time));
 
+      gsl_vector_fprintf(stdout, loc, "%f");
+      printf("The time is: %s\n",asctime(&time));
       printf("ground speed: %f\n", gps_speed);
       printf("course: %f\n", gps_course);
-/*
-      printf("fix qual: %d\n", gps_fix_quality);
-      printf("sats : %d\n", gps_satelites);
-      printf("hdop: %f\n", gps_hdop);
-      printf("alt : %f\n", gps_altitude);
-      printf("ellip : %f\n", gps_ellipsoid_seperation);
-*/
+      printf("fix quality: %d\n", gps_fix_quality);
+      printf("satelites: %d\n", gps_satelites);
+      printf("horizontal dilution of precision: %f\n", gps_hdop);
+      printf("altitude: %f\n", gps_altitude);
+      printf("ellipsoid seperation: %f\n", gps_ellipsoid_seperation);
+
     } else {
       printf("No fix\n");
       usleep(1000000);
