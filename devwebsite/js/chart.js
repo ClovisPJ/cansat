8979fcf9890a1ec037f0e891bd7cba0649f29f78
ClovisPJ <cpj234@gmail.com>
2016-02-06 17:44:33 +0000

    improved station added gauge

--- devwebsite/js/chart.js
+++ devwebsite/js/chart.js
@@ -1,69 +1,65 @@
 $(function () {
-    $(document).ready(function () {
-        $('#container').highcharts({
-            chart: {
-                type: 'spline',
-                animation: Highcharts.svg, // don't animate in old IE
-                events: {
-                    load: function () {
-                      var series = this.series[0];
-                      setInterval(function () {
-                        var xhttp = new XMLHttpRequest();
-                        xhttp.onreadystatechange = function() {
-                          if (xhttp.readyState == 4 && xhttp.status == 200) {
-                            var data = xhttp.responseText;
-                            console.log(data);
-                            series.addPoint(+data, true, true);
-                          }
+      $('#humidity').highcharts({
+          chart: {
+              type: 'spline',
+              events: {
+                  load: function () {
+                    var series = this.series[0];
+                    setInterval(function () {
+                      var xhttp = new XMLHttpRequest();
+                      xhttp.onreadystatechange = function() {
+                        if (xhttp.readyState == 4 && xhttp.status == 200) {
+                          var data = xhttp.responseText;
+                          console.log(data);
+                          series.addPoint(+data, true, true);
                         }
-                        xhttp.open("GET", "tmp", true);
-                        xhttp.send();
-                      }, 1000);
-                    }
-                }
-            },
-            title: {
-                text: 'Temperature of the Can',
-                x: -20 //center
-            },
-            xAxis: {
-              title: {
-                text: 'Time (seconds)'
-              }
-            },
-            yAxis: {
-                title: {
-                    text: 'Temperature (°C)'
-                },
-                plotLines: [{
-                    value: 0,
-                    width: 1,
-                    color: '#808080'
-                }]
-            },
-            tooltip: {
-                valueSuffix: '°C'
-            },
-            plotOptions: {
-                series: {
-                  animation: {
-                    duration: 0
+                      }
+                      xhttp.open("GET", "tmp", true);
+                      xhttp.send();
+                    }, 1000);
                   }
+              }
+          },
+          title: {
+              text: ''
+          },
+          xAxis: {
+            title: {
+              text: 'Time (seconds)'
+            }
+          },
+          yAxis: {
+              title: {
+                  text: 'Humidity'
+              },
+              plotLines: [{
+                  value: 0,
+                  width: 1,
+                  color: '#808080'
+              }]
+          },
+          tooltip: {
+              valueSuffix: '%'
+          },
+          plotOptions: {
+              series: {
+                animation: {
+                  duration: 0
                 }
-            },
-            exporting: {
-                enabled: false
-            },
-            series: [{
-                name: 'Temp',
-                data: (function () {
-                    var data = [], i;
-                    for (i = -19; i <= 0; i += 1) {
-                        data.push(1);
-                    }
-                    return data;
-                }())
-            }]
-        });
-    });
+              }
+          },
+          exporting: {
+              enabled: false
+          },
+          series: [{
+              showInLegend: false,
+              data: (function () {
+                  var data = [], i;
+                  for (i = -19; i <= 0; i += 1) {
+                      data.push(1);
+                  }
+                  return data;
+              }())
+          }]
+      });
 });
--- devwebsite/js/maps.js
+++ devwebsite/js/maps.js
@@ -1,10 +1,12 @@
 function initialize() {
-  var loc = new google.maps.LatLng(51.508742,-0.120850);
-  var loc1 = new google.maps.LatLng(55.508742,-1.570850);
-  var loc2 = new google.maps.LatLng(59.508742, 2.320850);
+  var loc = new google.maps.LatLng(52.481572, -0.468927);
+  var loc1 = new google.maps.LatLng(52.481997, -0.467972);
+  var loc2 = new google.maps.LatLng(52.483101, -0.467285);
+  var loc3 = new google.maps.LatLng(52.483441, -0.466432);
+  var loc4 = new google.maps.LatLng(52.482701, -0.466141);
   var mapProp = {
     center: loc,
-    zoom:6,
+    zoom:16,
     mapTypeId:google.maps.MapTypeId.ROADMAP
   };
   var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
@@ -12,7 +14,7 @@ function initialize() {
   var marker = new google.maps.Marker({ position: loc });
   marker.setMap(map);
 
-  var myTrip=[loc, loc1, loc2];
+  var myTrip=[loc, loc1, loc2, loc3, loc4];
   var flightPath = new google.maps.Polyline({
     path:myTrip,
     strokeColor:"#0000FF",
